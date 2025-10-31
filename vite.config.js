26352d0cbfd02420ddfe61c35b43ca2b32ea1312
ngogaroy <ngogaroy@gmail.com>
2025-10-31 11:25:22 +0300

    removed Speed-Insights

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from '@tailwindcss/vite'; // Import the Vite plugin for Tailwind
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  return {
-    plugins: [
-      react(),
-      tailwindcss(),
-    ],
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(), // <-- Add the Tailwind Vite plugin here
     
-    // This is the new, correct 'define' block
-    define: {
-      'process.env': {
-        NODE_ENV: JSON.stringify(mode),
-        // You can add any other env vars here if needed
-      }
-    },
-  };
+  ],
+  
 });
